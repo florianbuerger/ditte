8deb1be58565ab71cfe90bb82191fb7734306b58
Florian Buerger <florian@florianbuerger.com>
2014-05-16 11:50:07 +0200

    Initial Commit

---
+++ Ditte/DITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DITAppDelegate.h
+//  Ditte
+//
+//  Created by Florian Bürger on 16/05/14.
+//  Copyright (c) 2014 keslcod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ditte/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ditte
+//
+//  Created by Florian Bürger on 16/05/14.
+//  Copyright (c) 2014 keslcod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DITAppDelegate class]));
+    }
+}
